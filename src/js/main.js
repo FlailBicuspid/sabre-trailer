6ad2f43929edd100a85fc16ea8ab39a3c2330308
FlailBicuspid <winsteria@yahoo.com>
2025-11-11 14:18:11 +0100

    installation gsap et début d'animation du début

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,90 @@
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+// // PARALLAX-------------------------------------------
+
+// var tl = gsap.timeline ({
+//     // scrollTrigger: {
+//     //     trigger: ".section-parallax",
+//     //     start: "top top",
+//     //     end:"4000px bottom",
+//     //     scrub: 0.3,
+//     //     // pin: true,
+//     //     markers: true,
+//     // }
+// });
+
+
+gsap.to(
+    ".screen-b img",
+    {
+        y: "-40px",
+        scrollTrigger: {
+            trigger: ".screen-b",
+            start: "top 80%",
+            end:"bottom 40%",
+            scrub: 0.3,
+            // pin: true,
+            markers: true,
+        },
+    }
+);
+gsap.to(
+    ".screen-c img",
+    {
+        y: "-60px",
+        scrollTrigger: {
+            trigger: ".screen-c",
+            start: "top 90%",
+            end:"bottom 80%",
+            scrub: 0.3,
+            // pin: true,
+            markers: true,
+        },
+    }
+);
+gsap.to(
+    ".screen-d img",
+    {
+        y: "-300px",
+        scrollTrigger: {
+            trigger: ".screen-d",
+            start: "top 80%",
+            end:"bottom 80%",
+            scrub: 0.3,
+            // pin: true,
+            markers: true,
+        },
+    }
+);
+gsap.to(
+    ".screen-e img",
+    {
+        y: "-1900px",
+        scrollTrigger: {
+            trigger: ".screen-e",
+            start: "top 80%",
+            end:"bottom 80%",
+            scrub: 0.3,
+            // pin: true,
+            markers: true,
+        },
+    }
+);
+gsap.to(
+    ".screen-f img",
+    {
+        y: "-3000px",
+        scrollTrigger: {
+            trigger: ".screen-f",
+            start: "top 80%",
+            end:"bottom 80%",
+            scrub: 0.3,
+            // pin: true,
+            markers: true,
+        },
+    }
+);
+
